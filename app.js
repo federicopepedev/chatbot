4b386aaa5a9bd87952c4b5cb780b48837b611f96
Federico Pepe <federicopepedev@gmail.com>
2024-01-22 17:33:17 +0100

    built in bp

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const OpenAI = require("openai");
 const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
